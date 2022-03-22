73579d454f58ac480deffed7bbe1b9e8d998930f
arfcrrr <ajengarifa.char@gmail.com>
2022-03-22 16:51:14 +0700

    [ADD] TMDB API

---
+++ config/info.php
@@ -0,0 +1,13 @@
+<?php
+
+$apikey = "86e71646c28d8642157290f29fe5db6f";
+
+$sitename = "Overrated";
+$tagline = "Find your favourite movie rating here. Explore now or never!";
+$email = "ajengarifa.char@gmail.com";
+$url = "https://github.com/FSW-3-Grup-2/html-project";
+
+$imgurl_1 = "http://image.tmdb.org/t/p/w300";
+$imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+?>
